ef425c46a9ab612667a61edf433b63cb2082eeda
Alex Lowe <alex.lowe@canonical.com>
2025-10-14 23:06:23 -0400

    chore(experimental): core26 version

--- prepare-tests.sh
+++ prepare-tests.sh
@@ -7,7 +7,7 @@ snapcraft pack
 
 for path in $(find test/ -name snapcraft.yaml -exec dirname {} ';'); do
   pushd "${path}"
-  cp -alf $BASE_PATH/python-runtime-core24-312*.snap .
+  cp -alf $BASE_PATH/python-runtime-core26-314*.snap .
   snapcraft pack
   popd
 done
