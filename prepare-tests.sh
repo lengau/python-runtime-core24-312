e64bd44fc71447c91918eb29fdd3e7eac5e0fe2d
Alex Lowe <alex.lowe@canonical.com>
2025-10-14 21:37:10 -0400

    feat: initial snap and tests

---
+++ prepare-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+BASE_PATH="$(dirname "$(realpath $0)")"
+
+snapcraft pack
+
+for path in $(find test/ -name snapcraft.yaml -exec dirname {} ';'); do
+  pushd "${path}"
+  cp -alf $BASE_PATH/python-runtime-core24-312*.snap .
+  snapcraft pack
+  popd
+done
